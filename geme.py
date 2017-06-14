96efc68d4cfb386b41b8f0d2ec18d864b6f874d0
Elliot Fosong <efosong@gmx.com>
2017-06-14 17:12:44 +0100

    add month_to_int

--- geme.py
+++ geme.py
@@ -7,3 +7,8 @@ def norm_correlate(x,y):
     norm_x, norm_y = normalise(x), normalise(y)
     mag_x, mag_y = np.linalg.norm(norm_x), np.linalg.norm(norm_y)
     return np.correlate(norm_x, norm_y)/(mag_x*mag_y)
+
+def month_to_int(s):
+    s = "1960-02".split("-")
+    n = list(map(int, s))
+    return 12*n[0] + n[1]-1
