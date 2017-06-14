3183ce6c1b2f1a03d95f66decf5a8f3ef92b4e0f
Elliot Fosong <efosong@gmx.com>
2017-06-14 16:20:59 +0100

    normalise and norm_correlate functions

---
+++ geme.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def normalise(x):
+    return (x - x.mean())/x.std()
+
+def norm_correlate(x,y):
+    norm_x, norm_y = normalise(x), normalise(y)
+     mag_x,  mag_y = np.linalg.norm(norm_x), np.linalg.norm(norm_y)
+    return np.correlate(norm_x, norm_y)/(mag_x*mag_y)
