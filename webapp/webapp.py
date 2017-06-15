5e99e5ea0698f3bee8c14fdd3056103837a3fd30
mattcoates <mc955@cam.ac.uk>
2017-06-15 18:49:55 +0200

    messing with pngs

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response
 
 app = Flask(__name__)
 
@@ -16,6 +16,38 @@ def submission():
         # Call the correlation function here
         print(dataset)
         return # Serve results page template
+        
+        
+@app.route("/test.png")
+def simple():
+    import datetime
+    import io
+    import random
+
+    from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+    from matplotlib.figure import Figure
+    from matplotlib.dates import DateFormatter
+
+    fig=Figure()
+    ax=fig.add_subplot(111)
+    x=[]
+    y=[]
+    now=datetime.datetime.now()
+    delta=datetime.timedelta(days=1)
+    for i in range(10):
+        x.append(now)
+        now+=delta
+        y.append(random.randint(0, 1000))
+    ax.plot_date(x, y, '-')
+    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+    fig.autofmt_xdate()
+    canvas=FigureCanvas(fig)
+    png_output = io.BytesIO()
+    canvas.print_png(png_output)
+    response=make_response(png_output.getvalue())
+    response.headers['Content-Type'] = 'image/png'
+
+    return response
     
 
 
