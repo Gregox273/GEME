c63936a3911fcf51a6b3bdea9155ced5bef13cc2
Elliot Fosong <efosong@gmx.com>
2017-06-15 18:03:03 +0100

    calculate correlations in webapp

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,4 +1,5 @@
 import os
+from geme import *
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -12,9 +13,14 @@ def main():
 @app.route('/submit', methods=['GET'])
 def submission():
 
-        dataset = request.args.get('dataset')
+        dataset = r"../datasets/" + request.args.get('dataset') + ".csv"
+        df1 = read_data_frame(dataset)
+        df2 = read_data_frame(r"../datasets/realestate-loans-billions-monthl.csv") #TODO
+        r, sliced_df1, sliced_df2 = compare_datasets(df1, df2)
+        
         # Call the correlation function here
         print(dataset)
+        # use sliced_df1.plot() stuff to give a matplotlib plot object
         return # Serve results page template
     
 
