01579c44dd1c3b9d057d7f219c0013eb158a02e1
Gregory Brooks <gregox273@gmail.com>
2017-06-15 23:25:03 +0200

    Parse lat/lon from kml

--- spatial.py
+++ spatial.py
@@ -8,15 +8,21 @@ def read(filename):
    k.from_string(doc)
    return k
 
+def getLatLon(k, y, x, count=0):
+   if count > 20:
+      raise RuntimeError('Reached 20 recursions of getLatLon')
+   if not getattr(k, 'geometry',0):
+      #x,y = getLatLon(k.features, count+1)
+      for feature in k.features():
+         getLatLon(feature, y, x, count+1)
+   else:
+      x.append(k.geometry.x)
+      y.append(k.geometry.y)
+   return
+
+latitudes = []
+longitudes = []
 k = read("data/GIS/Licensed_Premises.kml")
-
-
-features = list(k.features())
-print(features)
-f2 = list(features[0].features())
-f3 = list(f2[0].features())
-#print(f3[0].geometry.x)
-#print(f3[0].geometry.y)
-
-#f4 = list(f3[0].geometry())
-#print(f4)
+getLatLon(k,latitudes,longitudes)
+print(latitudes)
+print(longitudes)
