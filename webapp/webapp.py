9d54c0b4b83c1e9bda03c704b2bc859beec26483
mattcoates <mc955@cam.ac.uk>
2017-06-16 13:17:39 +0200

    data

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 app = Flask(__name__)
 
 sliced_df1 = DataFrame([1])
+sliced_df2 = DataFrame([1])
 
 @app.route('/')
 def main(): 
@@ -17,25 +18,37 @@ def main():
 @app.route('/submit', methods=['GET'])
 def submission():
     global sliced_df1
+    global sliced_df2
 
-    dataset = r"../datasets/" + request.args.get('dataset') + ".csv"
+    dataset = r"../datasets/time/" + request.args.get('dataset')
     df1 = read_data_frame(dataset)
-    df2 = read_data_frame(r"../datasets/realestate-loans-billions-monthl.csv") #TODO
+    df2 = read_data_frame(r"../datasets/time/realestate-loans-billions-monthl.csv") #TODO
     r, sliced_df1, sliced_df2 = compare_datasets(df1, df2)
  
-    print(sliced_df1)
-    # use sliced_df1.plot(0) stuff to give a matplotlib plot object
     return render_template('timeresultspage.html')
         
         
-@app.route("/graph1.png") #TODO generalise this to prevent duplication in graph2, graph3, etc.
+@app.route("/graph1.png")
 def graph1():
-    # create plot
-    fig = plt.figure()
-    plt.plot(sliced_df1.index, sliced_df1.iloc[:,1])
-    #plt.xticks(range(len(sliced_df1.iloc[:,0])), sliced_df1.iloc[:,0])
     
-    # create png
+    # Create Figure
+    fig, ax1 = plt.subplots()
+
+    indices = sliced_df1.index if sliced_df1.index[0] == 0 else sliced_df2.index        
+
+    ax1.plot(indices, sliced_df1.iloc[:,1], 'b')
+    ax1.set_xlabel('Months')
+    ax1.set_ylabel('Datset 1', color='b')
+    ax1.tick_params('y', colors='b')
+
+    ax2 = ax1.twinx()
+    ax2.plot(indices, sliced_df2.iloc[:,1], 'r')
+    ax2.set_ylabel('Dataset 2', color='r')
+    ax2.tick_params('y', colors='r')
+
+    fig.tight_layout()
+    
+    # Create PNG
     canvas=FigureCanvas(fig)
     png_output = io.BytesIO()
     canvas.print_png(png_output)
@@ -46,12 +59,25 @@ def graph1():
     
 @app.route("/graph2.png")
 def graph2():
-    # create plot
-    fig = plt.figure()
-    plt.plot(sliced_df1.index, sliced_df1.iloc[:,1])
-    #plt.xticks(range(len(sliced_df1.iloc[:,0])), sliced_df1.iloc[:,0])
     
-    # create png
+    # Create Figure
+    fig, ax1 = plt.subplots()
+
+    indices = sliced_df1.index if sliced_df1.index[0] == 0 else sliced_df2.index        
+
+    ax1.plot(indices, sliced_df1.iloc[:,1], 'b')
+    ax1.set_xlabel('Months')
+    ax1.set_ylabel('Datset 1', color='b')
+    ax1.tick_params('y', colors='b')
+
+    ax2 = ax1.twinx()
+    ax2.plot(indices, sliced_df2.iloc[:,1], 'r')
+    ax2.set_ylabel('Dataset 2', color='r')
+    ax2.tick_params('y', colors='r')
+
+    fig.tight_layout()
+    
+    # Create PNG
     canvas=FigureCanvas(fig)
     png_output = io.BytesIO()
     canvas.print_png(png_output)
@@ -63,12 +89,25 @@ def graph2():
     
 @app.route("/graph3.png")
 def graph3():
-    # create plot
-    fig = plt.figure()
-    plt.plot(sliced_df1.index, sliced_df1.iloc[:,1])
-    #plt.xticks(range(len(sliced_df1.iloc[:,0])), sliced_df1.iloc[:,0])
     
-    # create png
+    # Create Figure
+    fig, ax1 = plt.subplots()
+
+    indices = sliced_df1.index if sliced_df1.index[0] == 0 else sliced_df2.index        
+
+    ax1.plot(indices, sliced_df1.iloc[:,1], 'b')
+    ax1.set_xlabel('Months')
+    ax1.set_ylabel('Datset 1', color='b')
+    ax1.tick_params('y', colors='b')
+
+    ax2 = ax1.twinx()
+    ax2.plot(indices, sliced_df2.iloc[:,1], 'r')
+    ax2.set_ylabel('Dataset 2', color='r')
+    ax2.tick_params('y', colors='r')
+
+    fig.tight_layout()
+    
+    # Create PNG
     canvas=FigureCanvas(fig)
     png_output = io.BytesIO()
     canvas.print_png(png_output)
@@ -76,16 +115,28 @@ def graph3():
     response.headers['Content-Type'] = 'image/png'
 
     return response
-    
     
 @app.route("/graph4.png")
 def graph4():
-    # create plot
-    fig = plt.figure()
-    plt.plot(sliced_df1.index, sliced_df1.iloc[:,1])
-    #plt.xticks(range(len(sliced_df1.iloc[:,0])), sliced_df1.iloc[:,0])
     
-    # create png
+    # Create Figure
+    fig, ax1 = plt.subplots()
+
+    indices = sliced_df1.index if sliced_df1.index[0] == 0 else sliced_df2.index        
+
+    ax1.plot(indices, sliced_df1.iloc[:,1], 'b')
+    ax1.set_xlabel('Months')
+    ax1.set_ylabel('Datset 1', color='b')
+    ax1.tick_params('y', colors='b')
+
+    ax2 = ax1.twinx()
+    ax2.plot(indices, sliced_df2.iloc[:,1], 'r')
+    ax2.set_ylabel('Dataset 2', color='r')
+    ax2.tick_params('y', colors='r')
+
+    fig.tight_layout()
+    
+    # Create PNG
     canvas=FigureCanvas(fig)
     png_output = io.BytesIO()
     canvas.print_png(png_output)
@@ -93,7 +144,6 @@ def graph4():
     response.headers['Content-Type'] = 'image/png'
 
     return response
-
 
     
 if __name__ == "__main__":
