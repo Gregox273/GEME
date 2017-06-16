df68b1be7f1c6af5b74d31420a81c241f1c7fa63
Elliot Fosong <efosong@gmx.com>
2017-06-16 08:05:00 +0100

    fix indentation

--- spatial.py
+++ spatial.py
@@ -1,24 +1,24 @@
 from fastkml import kml
 
 def read(filename):
-   with open(filename, 'rb') as kml_file:
-      doc = kml_file.read()
+    with open(filename, 'rb') as kml_file:
+        doc = kml_file.read()
 #   doc = (fame).read()
-   k = kml.KML()
-   k.from_string(doc)
-   return k
+    k = kml.KML()
+    k.from_string(doc)
+    return k
 
 def getLatLon(k, y, x, count=0):
-   if count > 20:
-      raise RuntimeError('Reached 20 recursions of getLatLon')
-   if not getattr(k, 'geometry',0):
-      #x,y = getLatLon(k.features, count+1)
-      for feature in k.features():
-         getLatLon(feature, y, x, count+1)
-   else:
-      x.append(k.geometry.x)
-      y.append(k.geometry.y)
-   return
+    if count > 20:
+        raise RuntimeError('Reached 20 recursions of getLatLon')
+    if not getattr(k, 'geometry',0):
+        #x,y = getLatLon(k.features, count+1)
+        for feature in k.features():
+            getLatLon(feature, y, x, count+1)
+    else:
+        x.append(k.geometry.x)
+        y.append(k.geometry.y)
+    return
 
 x1 = []
 y1 = []
